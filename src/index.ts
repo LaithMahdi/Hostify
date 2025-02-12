4eeb0702f401eb6526a9fb7562bbb04b267b6146
dalel-loussaief <loussaiefdalel15@gmail.com>
2025-02-12 15:46:34 +0100

    CRUD_Equipment

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import auth from "@/controller/auth_controller";
 const app = new Hono().basePath("/api/v1");
-
+import equipment from "@/controller/equiment_controller";
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
 app.route("/auth", auth);
-
+app.route("/Equipment", equipment);
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 
 console.log(`Server is running on http://localhost:${port}`);
